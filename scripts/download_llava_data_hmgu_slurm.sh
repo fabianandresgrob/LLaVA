bb255431d83bf7487fb9bf1651a5a436a4b0f7c8
Fabian Grob <grobfab@gmail.com>
2026-03-02 15:07:46 +0100

    Add cpu_normal QOS to download script

--- scripts/download_llava_data_hmgu_slurm.sh
+++ scripts/download_llava_data_hmgu_slurm.sh
@@ -6,6 +6,7 @@
 #SBATCH --mem=32G
 #SBATCH --time=12:00:00
 #SBATCH --partition=cpu_p
+#SBATCH --qos=cpu_normal
 
 # Requires $SCRATCH and $HF_HUB_CACHE to be set in ~/.bashrc:
 #   export SCRATCH=/lustre/groups/eml/projects/<username>
